07b6cbf4dfbee954b8cd6995b6afb0b40b8ba4c3
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-08-02 22:40:17 +0200

    I defined the routes and imported the Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout"
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route></Route>
+            <Route></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
