7624c7909d4e6fe02d6891b48d8de5efab9a4c4f
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-08-02 22:33:09 +0200

    added the different pages and components that will be used started injecting Outlet into layout

---
+++ src/components/layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+export default function layout() {
+    return (
+        <>
+            <nav></nav>
+            <main>
+                <Outlet />
+            </main>
+            <footer></footer>
+        </>
+    )
+}
